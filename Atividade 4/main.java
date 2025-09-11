e2ea98085e63d8e35b292d6cf8e12aa3cf42e8f7
Isadora Neves Rios <isadoraneves.dev@gmail.com>
2025-09-10 21:31:32 -0300

    Update main.java

--- Atividade 4/main.java
+++ Atividade 4/main.java
@@ -2,12 +2,13 @@ package Atividade4;
 
 public class main {
     public static void main(String[] args) {
-        Forma retangulo = new Retangulo(10, 5);
-        Forma quadrado = new Quadrado(7);
-        Forma circulo = new Circulo(4.5);
+        Forma retangulo = new Retangulo(6, 4);
+        Forma quadrado = new Quadrado(4);
+        Forma circulo = new Circulo(4.6);
 
         System.out.println("Área do retângulo: " + retangulo.calculaArea());
         System.out.println("Área do quadrado: " + quadrado.calculaArea());
         System.out.println("Área do círculo: " + circulo.calculaArea());
     }
 }
+
