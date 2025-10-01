832da056881ceca6f221f80b3c87d5401cb674c6
Isadora Neves Rios <isadoraneves.dev@gmail.com>
2025-09-30 22:00:42 -0300

    Update Main.java

---
+++ FactoryMethod/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Teste1 t1 = new Teste1(); // UDP
+        Teste2 t2 = new Teste2(); // TCP
+        Teste3 t3 = new Teste3(); // UDP
+
+        t1.getMessage();
+        t2.getMessage();
+        t3.getMessage();
+    }
+}
